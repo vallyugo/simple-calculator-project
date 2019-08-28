5b3ccd0395904675364403515f80d11b29eddf44
vallyugo <vallydike@gmail.com>
2019-08-28 12:48:00 +0100

    Complete html structure

---
+++ add.js
@@ -0,0 +1,27 @@
+var output = document.getElementById("result");
+
+function resolve(work) {
+    var pressed = work.innerHTML;
+    if (pressed == "=") {
+        output.innerHTML = eval(output.innerHTML);
+    } else if (pressed == "C") {
+        output.innerHTML = 0;
+    } else {
+        if (output.innerHTML == "0") {
+            if ((pressed == ".") || (pressed > 0)) {
+                output.innerHTML = pressed;
+            }
+        } else {
+
+            if (!isNaN(pressed)) {
+                output.innerHTML += pressed;
+            } else {
+                if (!isNaN(output.innerHTML.charAt(output.innerHTML.length - 1))) {
+                    output.innerHTML += pressed;
+                } else {
+                    output.innerHTML = output.innerHTML.slice(0, output.innerHTML.length - 1) + pressed;
+                }
+            }
+        }
+    }
+}
